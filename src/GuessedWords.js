1d9265b2b759dece3207e94db3abfb330b0f4700
Mohammed Nokri <mohammed.nokri@itg.co.uk>
2020-02-11 12:21:23 +0000

    Start tests for Guessed Words

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 
-const GuessedWords = (props) => {
+const GuessedWords = ({ guessedWords }) => {
+    let contents;
+    if (guessedWords.length === 0) {
+        contents = (
+            <div data-test="guess-instructions">
+                Try to guess the  secret word!
+            </div>
+        );
+    }
     return (
-        <div />
+        <div data-test="component-guessed-words">
+            { contents }
+        </div>
     );
 }
 
